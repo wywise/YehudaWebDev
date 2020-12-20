fd7cfde882d3b2a60a106512e4484f8c329d002a
wywise <y.weiss@outlook.com>
2020-12-20 23:39:20 +0200

    updates

--- YehudaWebDev/Startup.cs
+++ YehudaWebDev/Startup.cs
@@ -55,7 +55,7 @@ namespace YehudaWebDev
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Airlines}/{action=Index}/{id?}");
             });
         }
     }
